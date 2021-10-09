b99e6fb6df2c36fb8130c5666d0909eb5c1fde44
Ben Cuan <github@bencuan.me>
2021-10-09 11:51:11 -0700

    Improve rolling mechanics

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const ROLLS = [];
-const STATUSES = ['initiative', 'life', 'freedom', 'sheep', 'revenge', 'firstborn child', 'mortal enemy', 'strength', 'midterm grade', 'next snakpass gift', 'intelligence', 'seduction', 'strength', 'moustache quality'];
-const EMOJIS = ["😀","😃","😄","😁","😆","😅","😂","🤣","🥲","😊","😇","🙂","🙃","😉","😌","😍","🥰","😘","😗","😙","😚","😋","😛","😝","😜","🤪","🤨","🧐","🤓","😎","🥸","🤩","🥳","😏","😒","😞","😔","😟","😕","🙁","☹️","😣","😖","😫","😩","🥺","😢","😭","😤","😠","😡","🤬","🤯","😳","🥵","🥶","😱","😨","😰","😥","😓","🤗","🤔","🤭","🤫","🤥","😶","😐","😑","😬","🙄","😯","😦","😧","😮","😲","🥱","😴","🤤","😪","😵","🤐","🥴","🤢","🤮","🤧","😷","🤒","🤕","🤑","🤠","😈","👿","👹","👺","🤡","💩","👻","💀","☠️","👽","👾","🤖","🎃","😺","😸","😹","😻","😼","😽","🙀","😿","😾"]
+const STATUSES = ['initiative', 'life', 'freedom', 'sheep', 'revenge', 'firstborn child', 'mortal enemy', 'strength', 'midterm grade', 'next snakpass gift', 'intelligence', 'seduction', 'strength'];
+const EMOJIS = ["😀","😃","😄","😁","😆","😅","😂","🤣","🥲","😊","😇","🙂","🙃","😉","😌","😍","🥰","😘","😗","😙","😚","😋","😛","😝","😜","🤪","🤨","🧐","🤓","😎","🥸","🤩","🥳","😏","😒","😞","😔","😟","😕","🙁","☹️","😣","😖","😫","😩","🥺","😢","😭","😤","😠","😡","🤬","🤯","😳","🥵","🥶","😱","😨","😰","😥","😓","🤗","🤔","🤭","🤫","🤥","😶","😐","😑","😬","🙄","😯","😦","😧","😮","😲","🥱","😴","🤤","😪","😵","🤐","🥴","🤢","🤮","🤧","😷","🤒","🤕","🤑","🤠","😈","👿","👹","👺","🤡","💩","👻","💀","☠️","👽","👾","🤖","🎃","😺","😸","😹","😻","😼","😽","🙀","😿","😾"];
+const ROLL_TIME = 1500;
+const UPDATE_TIME = 75;
 /**
  * 0: completely random 2d6
  * 1: ordered bag of 2d6 rolls
@@ -20,22 +22,24 @@ const genRolls = (mode) => {
 genRolls();
 
 document.getElementById('roll-btn').addEventListener('click', () => {
-    document.getElementById('roll-btn').innerHTML = `roll for your ${STATUSES[Math.floor(Math.random() * STATUSES.length)]}`
+    document.getElementById('roll-btn-content').innerHTML = `roll for your ${STATUSES[Math.floor(Math.random() * STATUSES.length)]}`;
+    document.getElementById('roll-btn-emoji').classList.add('spinning');
     if (ROLLS.length === 0) {
         genRolls();
     }
     let num = ROLLS.pop();
+    let refreshes = Math.floor(ROLL_TIME / UPDATE_TIME);
     let interval = setInterval(() => {
+      if (refreshes > 0) {
         document.getElementById('roll-out').innerHTML = `Current roll: ${EMOJIS[Math.floor(Math.random() * EMOJIS.length)]}`;
-    }, 100);
-    setTimeout(() => {
-        clearInterval(interval); 
-    }, 800);
-    setTimeout(() => {
+        refreshes -= 1;
+      } else {
+        clearInterval(interval);
         console.log(ROLLS);
         document.getElementById('roll-out').innerHTML = `Current roll: ${num}`;
         document.getElementById('roll-left').innerHTML = `Rolls left in bag: ${ROLLS.length}`;
-    }, 1200);
-
+        document.getElementById('roll-btn-emoji').classList.remove('spinning');
+      }
+    }, UPDATE_TIME);
 })
 
